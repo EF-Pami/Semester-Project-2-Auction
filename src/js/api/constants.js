1b17cf96ea6e85b0206c89f5e7152d0d42433bf4
EF-Pami <pamijoelpami@gmail.com>
2024-10-24 21:09:58 +0200

    added constant.js file

---
+++ src/js/api/constants.js
@@ -0,0 +1,12 @@
+const API_BASE_URL = "https://api.noroff.dev/api/v1/auction/";
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+export const listingsURL = `${API_BASE_URL}listings`;
+export const allListingsURL = `${listingsURL}?_active=true&_bids=true&_seller=true`;
+export const listingDetailsURL = `${listingsURL}/${id}?_seller=true&_bids=true`;
+export const bidsURL = `${API_BASE_URL}listings/${id}/bids/`;
+export const authURL = `${API_BASE_URL}auth/`;
+export const profilesURL = `${API_BASE_URL}profiles/`;
