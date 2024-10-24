7b27fa9d10ee6662587d676700502661c84987d8
EF-Pami <pamijoelpami@gmail.com>
2024-10-24 21:25:03 +0200

    added bootstrap validation

---
+++ src/js/components/closeAccordion.js
@@ -0,0 +1,20 @@
+/**
+ * Closes the accordion item with the id 'update-collapse' if it is currently open.
+ * Also updates the 'aria-expanded' attribute of the corresponding button.
+ * @example
+ * ```js
+ * closeAccordion();
+ * ```
+ */
+export function closeAccordion() {
+    const accordionItem = document.querySelector("#update-collapse");
+    const accordionButton = document.querySelector(
+      '[data-bs-target="#update-collapse"]',
+    );
+  
+    if (accordionItem.classList.contains("show")) {
+      accordionItem.classList.remove("show");
+      accordionButton.setAttribute("aria-expanded", "false");
+    }
+  }
+  
