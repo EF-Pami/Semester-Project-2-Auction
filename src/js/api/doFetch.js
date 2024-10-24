933378433a0af394c0ce8a4b4bf8638387d79bf8
EF-Pami <pamijoelpami@gmail.com>
2024-10-24 21:10:26 +0200

    added fetching options

---
+++ src/js/api/doFetch.js
@@ -0,0 +1,27 @@
+/**
+ * Performs a fetch request to the specified URL with the given options.
+ * @param {string} url - The URL to fetch.
+ * @param {Object} [customOptions={}] - The custom options to include in the fetch request. Defaults to an empty object.
+ * @returns {Promise} - The JSON response from the fetch request.
+ * @throws {Error} Will throw an error if the fetch request fails.
+ * @example
+ *
+ */
+export async function doFetch(url, customOptions = {}) {
+    try {
+      const defaultOptions = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        ...customOptions,
+      };
+      const response = await fetch(url, defaultOptions);
+      const json = await response.json();
+  
+      return json;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  
---
+++ src/js/api/fetchOptions.js
@@ -0,0 +1,89 @@
+import { getStorage } from "../storage/getStorage.js";
+
+const token = getStorage("accessToken");
+
+/**
+ * Options for a GET request.
+ * @type {Object}
+ * @property {string} method - The HTTP method.
+ * @property {Object} headers - The headers to include in the request.
+ * @property {string} headers.Content-Type - The media type of the resource.
+ * @property {string} headers.Authorization - The authorization token.
+ */
+export const getData = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+};
+
+/**
+ * Options for a DELETE request.
+ * @type {Object}
+ * @property {string} method - The HTTP method.
+ * @property {Object} headers - The headers to include in the request.
+ * @property {string} headers.Content-Type - The media type of the resource.
+ * @property {string} headers.Authorization - The authorization token.
+ */
+export const deleteData = {
+  method: "DELETE",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+};
+
+/**
+ * Creates options for a POST request with the given data.
+ * @param {Object} data - The data to be sent in the request body.
+ * @returns {Object} - The options for the POST request.
+ * @example
+ * ```js
+ * const data = {
+ *   title: 'New item',
+ *   description: 'This is a new item',
+ * };
+ *
+ * const options = postData(data);
+ * ```
+ */
+export function postData(data) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  };
+
+  return options;
+}
+
+/**
+ * Creates options for a PUT request with the given data.
+ * @param {Object} data - The data to be sent in the request body.
+ * @returns {Object} - The options for the PUT request.
+ * @example
+ * ```js
+ * const data = {
+ *   title: 'Updated item',
+ *   description: 'This is an updated item',
+ * };
+ *
+ * const options = putData(data);
+ * ```
+ */
+export function putData(data) {
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  };
+
+  return options;
+}
