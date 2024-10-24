4142c6b696c7fb516f4da9c88ae1667b4921c270
EF-Pami <pamijoelpami@gmail.com>
2024-10-24 21:17:47 +0200

    added lising update

---
+++ src/js/components/displayListingUpdateForm.js
@@ -0,0 +1,12 @@
+/**
+ * Displays the listing update form by removing the "visually-hidden" class from the form container.
+ */
+export function displayListingUpdateForm() {
+    const listingUpdateForm = document.querySelector("#updateListingContainer");
+    const listingUpdateFormButton = document.querySelector(
+      "#updateListingButton",
+    );
+    listingUpdateForm.classList.remove("visually-hidden");
+    listingUpdateFormButton.classList.remove("visually-hidden");
+  }
+  
