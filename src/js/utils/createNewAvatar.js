5ee76bae5d438c66e2fc4cb2472cfebdb45949ad
EF-Pami <pamijoelpami@gmail.com>
2024-10-24 21:26:31 +0200

    added create new bid, avatar

---
+++ src/js/utils/createNewAvatar.js
@@ -0,0 +1,20 @@
+/**
+ * Creates a new avatar object from the value of the "changeAvatar" input field in the "changeAvatarForm" form.
+ * @returns {Object} An object with the new avatar URL.
+ * @example
+ * ```js
+ * const newAvatar = createNewAvatar();
+ * ```
+ */
+export function createNewAvatar() {
+    const form = document.querySelector("#changeAvatarForm");
+    const formInputValue = form.changeAvatar.value;
+    const newAvatarUrl = formInputValue;
+  
+    const newAvatar = {
+      avatar: newAvatarUrl,
+    };
+  
+    return newAvatar;
+}
+  
---
+++ src/js/utils/createNewBid.js
@@ -0,0 +1,20 @@
+/**
+ * Creates a new bid object from the value of the "addBidAmount" input field in the "addBidForm" form.
+ * @returns {Object} An object with the new bid amount.
+ * @example
+ * ```js
+ * const newBid = createNewBid();
+ * ```
+ */
+export function createNewBid() {
+    const form = document.querySelector("#addBidForm");
+    const formInputValue = form.addBidAmount.value;
+    const bidAmount = parseInt(formInputValue);
+  
+    const bid = {
+      amount: bidAmount,
+    };
+  
+    return bid;
+}
+  
